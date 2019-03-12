12790217c03db03b3a91da017be6f0e8c4df129f
Nikolay Dikarev <motoniko@Nikolay.local>
2019-03-12 20:32:56 +0300

    update pages
    кроме signup/accounts/contacts/payment

--- js/plus7days.js
+++
@@ -1,5 +0,0 @@
-const date = Date.now() ;
-const datePlus7 = new Date(date+(7*24*60*60*1000));
-const  noDayname  = datePlus7.toDateString().substr(4,6);
-trialCount  = document.getElementById('trialCount')
-trialCount.innerText = datePlus7.toDateString().substr(4,6);
