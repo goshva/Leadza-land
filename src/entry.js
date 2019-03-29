4dc5f674fcece147cdef8b85b7aca8f810a98366
Anton Ignatev <nslsmain@gmail.com>
2019-03-30 00:54:44 +0200

    Fix bundle path for testing

--- src/entry.js
+++ src/entry.js
@@ -3,7 +3,8 @@ import "./styles/index.scss";
 
 $(document).ready(() => {
   const initializersMap = {
-    "/payment2": payment
+    "/payment2": payment,
+    "/payment2.html": payment
   };
 
   const path = window.location.pathname;
