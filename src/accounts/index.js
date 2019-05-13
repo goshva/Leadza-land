4019dc0aa3252df32bbdd4f053d14ed7279ede32
goshva <sha.egor@gmail.com>
2019-05-13 10:25:07 +0000

    renew test

--- src/accounts/index.js
+++ src/accounts/index.js
@@ -5,8 +5,13 @@ export default function initAccounts() {
   let accountsList;
   let tryings = 0;
   
-//  setTimeout(() => {
       getSettings();
+  setTimeout(() => {
+$('#form98281816').each(function(){
+        $(this).data('success-callback', 'window.mySuccessFunction');
+    });
+
+}, 1700);
 
 //      $(".js-form-proccess").each(function() {
 //        $(this).data("success-callback", "window.mySuccessFunction");
@@ -16,23 +21,19 @@ export default function initAccounts() {
  
 //     document.body.style.cursor = "wait";
 //      document.getElementsByClassName("loadfreeze")[0].style.display = "block";
-//}, 700);
 
   window.mySuccessFunction = () => {
     const selector = document.getElementsByName("ad_list_option")[0];
     const value = selector[selector.selectedIndex].value;
+    const id  = getValue(parseName(value));
+    cookier.setCookie("firstAccount", id,{expires:3600});
+    cookier.setCookie("firstAccountSpend", getSpendbyID(id),{expires:3600});
+    cookier.setCookie("firstCampsList", getCamps(id),{expires:3600});
 
-    console.log(value)
-    cookier.setCookie("firstAccount", value,{expires:3600});
-    cookier.setCookie("firstAccountSpend", getSpendbyID(value),{expires:3600});
-    cookier.setCookie("firstCampsList", getCamps(value),{expires:3600});
-
-//    window.location.href = "/_onboarding";
+    window.location.href = "/_onboarding";
   };
 
   function getSettings() {
-    document.body.style.cursor = "wait";
-    document.getElementsByClassName("loadfreeze")[0].style.display = "block";
     fetch(`/api/user/${userID}/settings`, {
       method: "GET",
       headers: {
@@ -59,8 +60,8 @@ export default function initAccounts() {
           }, 2000);
         } else {
           addOptions(accountsList);
-          document.getElementsByClassName("loadfreeze")[0].style.display =
-            "none";
+          reInit();
+          document.getElementsByClassName("loadfreeze")[0].style.display = "none";
         }
       })
       .catch(function() {
@@ -70,50 +71,65 @@ export default function initAccounts() {
 
   function addOptions(inputdata) {
   let arra = inputdata.map((element) => {
-      var str = element.name+" $"+element.last_month_spend_usd+"/month" 
+      var str = element.name+" $"+element.last_month_spend_usd+"/month."
       return str;
   });
   arra = arra.join('\n')
   let textareaDefault = JSON.parse(document.getElementsByTagName("textarea")[0].value);
+  console.log(textareaDefault);
   textareaDefault[0].li_variants = arra;
   document.getElementsByTagName("textarea")[0].value = JSON.stringify(textareaDefault);
-  console.log(arra);
 
-    var option;
-    var select = document.querySelector("select[name=ad_list_option]");
-    select.options.length = 1;
-    console.log(inputdata);
-    inputdata.forEach(function(item) {
-      option = document.createElement("option");
-      option.textContent = ` ${item.name} $${item.last_month_spend_usd}/month`;
-      option.value = item.id;
-      console.log(option);
-      if (typeof(option.value) !== 'undefined'){       select.appendChild(option);  }
-    });
+//    var option;
+//    var select = document.querySelector("select[name=ad_list_option]");
+//    select.options.length = 1;
+//    select.addEventListener('change',function(){alert(10) });
+///    console.log(inputdata);
+//    inputdata.forEach(function(item) {
+//      option = document.createElement("option");
+//      option.textContent = ` ${item.name} $${item.last_month_spend_usd}/month`;
+//      option.value = item.id;
+//      console.log(option);
+//      if (typeof(option.value) !== 'undefined'){       select.appendChild(option);  }
+//    });
   }
   function getSpendbyID(value) {
     const acc = accountsList.find(acc => acc.id == value);
     return acc.last_month_spend_usd;
   }
   function getCamps(value) {
-    const acc = accountsList.find(acc => `${acc.id}` === value);
+    const acc = accountsList.find(acc => acc.id === value);
     const camps = acc.campaigns.map(a => a.id);
+    console.log(camps);
     return JSON.stringify(camps);
   }
 
-  function getValue(str) {
-    const acc = accountsList.find(acc => acc.id == value);
-    return acc.last_month_spend_usd;
+  function getValue(name){
+    console.log(name);
+    const acc = accountsList.find(acc => acc.name == name);
+    return acc.id;
   }
-//$('button[type="submit"]').on("click", e => {
- //   e.preventDefault();
+  function parseName(str) {
+    console.log(str);
+    return str.split(' \$')[0]
+  }
 
-//    submit();
+function reInit(){
+//        $(this).data("success-callback", "window.mySuccessFunction");
+//        $(this).attr("data-success-callback", "window.mySuccessFunction");
+//        $(this).attr("data-success-url", "")
+$('#form98281816').each(function(){
+        $(this).data('success-callback', 'window.mySuccessFunction');
+    });
+  window.dispatchEvent(new Event('resize'));
+//  const selector = document.getElementsByName("ad_list_option")[0];
+//  const value = selector[selector.selectedIndex].value;
+//var selector =  document.querySelector('select[name="ad_list_option"]');
+//selector.addEventListener('change',function(){
+//    alert('changed');
 //});
 
-//$(".js-form-proccess").each(function() {
-///    $(this).data("success-callback", "window.mySuccessFunction");
-//    $(this).attr("data-success-callback", "window.mySuccessFunction");
-//    $(this).attr("data-success-url", "");
-//});
+  console.log(selector)
+  console.log(value)
+};
 }
